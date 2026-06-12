4f35ff350a5b67e827fcf472b4fefc713b28d903
arahel <abhishekrahel@example.com>
2026-06-12 15:14:33 +0530

    docker

---
+++ Docker/java-docker-app/main.java
@@ -0,0 +1,26 @@
+// Main.java
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Start a lightweight HTTP server on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from Dockerized Java App!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        server.start();
+        System.out.println("Server is running on port 8080...");
+    }
+}
